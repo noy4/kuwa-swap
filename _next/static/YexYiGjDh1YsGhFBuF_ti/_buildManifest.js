2f568d445a4ce71a5fe9f08ae963c03b1d047447
noy4 <noy4@users.noreply.github.com>
2022-01-13 03:55:07 +0000

    deploy: 194a1f4c42b8b76a71aca82053c4b45a07d6ddeb

---
+++ _next/static/YexYiGjDh1YsGhFBuF_ti/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/593-980174caabc84d83.js","static/chunks/pages/index-83dee721df83e578.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
