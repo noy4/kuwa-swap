0b00bd67d1db6df7ce2841b38a0d0f9fe80b7138
noy4 <naoyan418@gmail.com>
2022-01-13 12:47:22 +0900

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
+const basePath = isProd ? '/kuwa-swap' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath,
+  assetPrefix: basePath,
+  env: {
+    basePath,
+  },
 }
 
 module.exports = nextConfig
