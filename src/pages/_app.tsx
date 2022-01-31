391bad98f2566096ccfb99132ed9d3f05fb2b474
noy4 <naoyan418@gmail.com>
2022-02-01 04:58:25 +0900

    initializeOnMount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { MoralisProvider } from 'react-moralis'
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <MoralisProvider
+      initializeOnMount
       appId={process.env.NEXT_PUBLIC_APP_ID ?? ''}
       serverUrl={process.env.NEXT_PUBLIC_SERVER_URL ?? ''}
     >
